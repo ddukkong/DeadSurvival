415cb1a706d255c0a3c712b8f47ada64cea228f7
ddukkong <95466023+ddukkong@users.noreply.github.com>
2022-12-07 15:04:40 +0900

    update2

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public float spawnTime = 3f;
+    public float spawnTime = 1f;
+    public float BossSpawnTime = 5f;
     public float curTime;
+    public float BosscurTime;
     public Transform[] spawnPoints;
     public GameObject enemy0;
     public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
 
     void Update()
     {
@@ -18,7 +22,13 @@ public class SpawnManager : MonoBehaviour
             SpawnEnemy(x);
             
         }
+        if(BosscurTime >= BossSpawnTime)
+        {
+            int x = Random.Range(0, spawnPoints.Length);
+            BossSpawnEnemy(x);
+        }
         curTime += Time.deltaTime;
+        BosscurTime += Time.deltaTime;
 
     }
     public void SpawnEnemy(int RanNum)
@@ -26,9 +36,12 @@ public class SpawnManager : MonoBehaviour
         curTime = 0;
         Instantiate(enemy0, spawnPoints[RanNum]);
         Instantiate(enemy1, spawnPoints[RanNum]);
-    
-    
-    
+    }
+    public void BossSpawnEnemy(int RanNum)
+    {
+        BosscurTime = 0;
+        Instantiate(enemy2, spawnPoints[RanNum]);
+        Instantiate(enemy3, spawnPoints[RanNum]);
     }
 
 
